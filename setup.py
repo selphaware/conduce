86a37836cb9e216e57de87acaa7574d708cae21c
selphaware-ahmadus <uahmad3013@outlook.com>
2021-08-23 00:55:28 +0100

    uptick version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="conduce",
-    version="0.0.1",
+    version="0.0.2",
     author="Usman Ahmad",
     author_email="uahmad3013@outlook.com",
     description="A config util package to flatten-reduce long keys",
