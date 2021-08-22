24b46c3405fd96ec641e18d1c1fea369734cc586
selphaware-ahmadus <uahmad3013@outlook.com>
2021-08-22 11:14:02 +0100

    update readme, git page, and description

--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ setuptools.setup(
     description="A config util package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/selphaware/conduce",
     project_urls={
-        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+        "Bug Tracker": "https://github.com/selphaware/conduce/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
